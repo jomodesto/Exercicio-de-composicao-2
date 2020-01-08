4ad4ee0c2bef773437734fac588881fe6ec13cd7
Jonatas <modesto.jonatas18@gmail.com>
2020-01-08 18:31:01 -0300

    Projeto de Post e Comentario

---
+++ Composição exercício resolvido 2/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Composição_exercício_resolvido_2.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+    }
+    
+}
